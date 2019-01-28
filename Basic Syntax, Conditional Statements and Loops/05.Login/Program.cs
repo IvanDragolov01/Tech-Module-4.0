bcee4d5b1e7bf239809e513b35ee8357a34b70da
IvanDragolov <sinkara23@abv.bg>
2019-01-28 10:29:28 +0200

    add second solution to ex 5

--- Basic Syntax, Conditional Statements and Loops/05.Login/Program.cs
+++ Basic Syntax, Conditional Statements and Loops/05.Login/Program.cs
@@ -34,4 +34,38 @@ namespace _05.Login
 			return new string(arr);
 		}
 	}
+
+	//second solution
+	//	class Program
+	//{
+	//	static void Main(string[] args)
+	//	{
+	//		string username = Console.ReadLine();
+	//		string password = "";
+
+	//		for (int i = username.Length - 1; i >= 0; i--)
+	//		{
+	//			password += username[i];
+	//		}
+
+	//		string saveusername = Console.ReadLine();
+	//		int br = 1;
+
+	//		while (password != saveusername)
+	//		{
+	//			Console.WriteLine("Incorrect password. " + "Try again.");
+	//			saveusername = Console.ReadLine();
+	//			br++;
+
+	//			if (br == 4 && password != saveusername)
+	//			{
+	//				Console.WriteLine($"User {username} blocked!");
+	//				return;
+	//			}
+	//		}
+
+	//		Console.WriteLine($"User {username} logged in.");
+	//	}
+	//}
 }
+
