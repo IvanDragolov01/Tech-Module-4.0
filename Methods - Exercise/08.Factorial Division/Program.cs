2dad0760b2e526a5de829cc380617837c7bcb2be
IvanDragolov <sinkara23@abv.bg>
2019-02-06 14:53:25 +0200

    add ex 8

---
+++ Methods - Exercise/08.Factorial Division/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _8._Factorial_Division
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			long firstNumber = long.Parse(Console.ReadLine());
+			long secondNumber = long.Parse(Console.ReadLine());
+			double result = CalculateFactorial(firstNumber) / CalculateFactorial(secondNumber);
+			Console.WriteLine($"{result:F2}");
+		}
+
+		public static double CalculateFactorial(long a)
+		{
+			if (a == 0)
+			{
+				return 1;
+			}
+			else
+			{
+				return a * CalculateFactorial(a - 1);
+			}
+		}
+	}
+}
