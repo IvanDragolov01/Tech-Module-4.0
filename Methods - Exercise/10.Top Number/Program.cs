583a3b3548d1fa5d19157900555c3aacd9c52c18
IvanDragolov <sinkara23@abv.bg>
2019-02-15 14:52:30 +0200

    add ex 10

---
+++ Methods - Exercise/10.Top Number/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _10.Top_Number
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int startIndex = int.Parse(Console.ReadLine());
+			int sumOfDigits = 0;
+			int br = 0;
+			Digits(startIndex, sumOfDigits, br);
+		}
+
+		private static void Digits(int startIndex, int sumOfDigits, int br)
+		{
+			for (int i = 1; i <= startIndex; i++)
+			{
+				int p = i;
+
+				sumOfDigits = 0;
+				br = 0;
+
+				while (p > 0)
+				{
+					int num = p % 10;
+					sumOfDigits += num;
+
+					if (num % 2 != 0)
+					{
+						br++;
+					}
+
+					p /= 10;
+				}
+
+				if (sumOfDigits % 8 == 0 && br >= 1)
+				{
+					Console.WriteLine(i);
+				}
+			}
+		}
+	}
+}
