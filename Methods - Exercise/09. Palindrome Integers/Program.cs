c9533249004c520b613ca449f3be1ffe4f86b9fd
IvanDragolov <sinkara23@abv.bg>
2019-02-15 14:51:53 +0200

    add ex 9

---
+++ Methods - Exercise/09. Palindrome Integers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _09._Palindrome_Integers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string command;
+
+			while ((command = Console.ReadLine()) != "END")
+			{
+				if (command == Reverse(command))
+				{
+					Console.WriteLine("true");
+				}
+				else
+				{
+					Console.WriteLine("false");
+				}
+			}
+		}
+
+		public static string Reverse(string str)
+		{
+			char[] charArray = str.ToCharArray();
+			Array.Reverse(charArray);
+			return new string(charArray);
+		}
+	}
+}
